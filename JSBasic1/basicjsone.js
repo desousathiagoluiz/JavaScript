6f32ecfc464f4c7b1c350a7d7a6917a608d99214
ThiaGão <81779698+desousathiagoluiz@users.noreply.github.com>
2021-12-19 23:11:44 -0300

    Add files via upload

---
+++ JSBasic1/basicjsone.js
@@ -0,0 +1,208 @@
+// Tabuada: Laço de Repetição. (Incremento {FOR // INSTRUÇÃO} - Multiplas Condições com Continuação).
+let n = 9;
+
+for (let i = 0; i <= 10; i++) {
+
+    console.log(`${i} X ${n} = ${i * n}`);
+    // Concatenação de Variáveis e Valores
+    /*console.log(i + "X" + n + "=" + (i * n));*/
+
+}
+
+
+
+// Laço de Repetição. (Incremento {SWITCH} - Multiplas Condições com Quebra).
+/*let cor = "azul";
+
+switch (cor) {
+
+    case "verde":
+        console.log("siga");
+        break;
+
+    case "amarelo":
+        console.log("atenção");
+        break;
+
+    case "vermelho":
+        console.log("pare");
+        break;
+
+    default:
+        console.log("A Cor não é azul.");
+}*/
+
+
+
+// Laço de Repetição. (Incremento - Multiplas Condições com Retorno Falso)
+/*let cor = "azul";
+
+if (cor === "verde") {
+
+    console.log("siga");
+
+} else if (cor === "amarelo") {
+
+    console.log("atenção");
+
+} else if (cor === "vermelho") {
+
+    console.log("pare");
+
+} else {
+
+    console.log("Seja Feliz me CONTRATANDO!! :D");
+
+}*/
+
+
+
+// Laço de Repetição. (Incremento - Multiplas Condições com Retorno Falso)
+/*let cor = "azul";
+
+if (cor === "verde") {
+
+    console.log("siga");
+
+} else if (cor === "amarelo") {
+
+    console.log("atenção");
+
+} else if (cor === "vermelho") {
+
+    console.log("pare");
+
+} else {
+
+    console.log("Seja Feliz me CONTRATANDO!! :D");
+
+}*/
+
+
+
+// Laço de Repetição. (Incremento - Multiplas Condições)
+/*let cor = "amarelo";
+
+if (cor === "verde") {
+
+    console.log("siga");
+
+} else if (cor === "amarelo") {
+
+    console.log("atenção");
+
+} else {
+
+    console.log("pare");
+
+}*/
+
+
+// Laço de Repetição. (Incremento - Condição Falsa)
+/*let cor = "vermelho";
+
+if (cor === "verde") {
+
+    console.log("siga");
+
+} else {
+
+    console.log("pare");
+
+}*/
+
+
+
+// Laço de Repetição. (Incremento - Condição Verdadeira)
+/*let cor = "verde";
+
+if (cor === "verde") {
+
+    console.log("siga");
+
+} else {
+
+    console.log("pare");
+
+}*/
+
+
+
+// Operadores Lógicos e Condição. (Pipes OR)
+/*let a = 10;
+const b = "10";
+
+console.log(a == b || typeof a == 'string');*/
+
+
+// Operadores Lógicos e Condição. (Pipes OR)
+/*let a = 10;
+const b = "10";
+
+console.log(a == b || typeof b == 'string');*/
+
+
+// Operadores Lógicos e Condição. (Valor 'a' - "string?" AND)
+/*let a = 10;
+const b = "10";
+
+console.log(a == b && typeof a == 'string');*/
+
+
+// Operadores Lógicos e Condição. (Valor 'b' - "string?" AND)
+/*let a = 10;
+const b = "10";
+
+console.log(a == b && typeof b == 'string');*/
+
+
+// Diferença entre Valor e Tipo de Dados. (Atribuição)
+/*let a = 10;
+const b = "10";
+
+console.log(a != b);*/
+
+
+// Diferença entre Conteúdo e Tipo de Dados. (Comparação)
+/*let a = 10;
+const b = "10";
+
+console.log(a !== b);*/
+
+
+// Validando Conteúdo e Tipo de Dados.
+/*let a = 10;
+const b = "10";
+
+console.log(a === b);*/
+
+
+// Comparação entre Variável(let) e Constante(const): Valores Númericos Iguais e String.
+/*let a = 10;
+const b = "10";
+
+console.log(a == b);*/
+
+
+// Comparação entre Variável(let) e Constante(const): Valor Númerico Diferente e String.
+/*let a = 10;
+const b = "20";
+
+console.log(a == b);*/
+
+
+// Comparação entre Variável(let) e Constante(const): Valores Númericos. 
+/*let a = 10;
+const b = 20;
+
+console.log(a == b);*/
+
+
+
+// Testando Variável e Imprimindo "Olá Mundo!".
+/*var helloWord = "Olá Mundo!";
+
+
+console.log(helloWord);
+console.log(helloWord);
+console.log(helloWord);*/
